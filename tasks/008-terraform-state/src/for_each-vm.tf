c50218b0242de0fa9ade5ee69664bf3b4f26a9cd
Ivan Podovalov <i.podovalov@rdp.ru>
2023-06-08 19:37:52 +0300

    ter-homemorks: task #2.2

---
+++ tasks/008-terraform-state/src/for_each-vm.tf
@@ -0,0 +1,37 @@
+locals {
+  virtual_machines = {
+    "vm1" = { vm_name = "main", vm_cpu=2, vm_ram=1, vm_disk_size=1, vm_core_fraction=5 },
+    "vm2" = { vm_name = "replica", vm_cpu=2, vm_ram=1, vm_disk_size=1, vm_core_fraction=20 }
+  }
+}
+
+resource "yandex_compute_instance" "servers" {
+
+  for_each = local.virtual_machines
+
+  name        = each.value.vm_name
+  platform_id = "standard-v1"
+  resources {
+    cores         = each.value.vm_cpu
+    memory        = each.value.vm_ram
+    core_fraction = each.value.vm_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.os_image.image_id
+      size = each.value.vm_disk_size
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+  metadata = {
+    serial-port-enable = var.vm_metadata.serial-port-enable
+    ssh-keys           = var.vm_metadata.ssh-keys
+  }
+
+}
