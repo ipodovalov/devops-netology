9b9517cc32073e08c949b33bbdaea4cd4a4a113a
Ivan Podovalov <i.podovalov@rdp.ru>
2023-06-12 14:29:25 +0300

    ter-homemorks: task #3.2

--- tasks/008-terraform-state/src/disk_vm.tf
+++ tasks/008-terraform-state/src/disk_vm.tf
@@ -1,8 +1,49 @@
-resource "yandex_compute_disk" "empty-disk" {
+resource "yandex_compute_disk" "empty_disk" {
   count      = 3
   name       = "empty-disk-${count.index+1}"
   type       = "network-hdd"
   zone       = var.default_zone
   size       = 1
   block_size = 4096
+}
+
+resource "yandex_compute_instance" "disk-vm" {
+
+  depends_on = [yandex_compute_disk.empty_disk]
+
+  name        = "netology-disk-vm"
+  platform_id = "standard-v1"
+
+  resources {
+    cores         = var.vm_minimal.cores
+    memory        = var.vm_minimal.memory
+    core_fraction = var.vm_minimal.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.os_image.image_id
+    }
+  }
+
+  dynamic "secondary_disk" {
+    for_each = toset(yandex_compute_disk.empty_disk[*].id)
+    content {
+      disk_id     = secondary_disk.key
+      auto_delete = true
+    }
+  }
+
+  metadata = local.ssh_keys_and_serial_port
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+  allow_stopping_for_update = true
+
 }
