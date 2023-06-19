c8c19d3cd25334ee5037fe95dc823f21c2d34888
Ivan Podovalov <i.podovalov@rdp.ru>
2023-06-19 14:26:25 +0300

    ter-homemorks: task #2.3

--- tasks/009-terraform-advan/src/vpc/main.tf
+++ tasks/009-terraform-advan/src/vpc/main.tf
@@ -1,15 +1,15 @@
-resource "yandex_vpc_network" "test_vpc" {
+resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
   description = "vpc description"
 
   labels = var.labels
 }
-resource "yandex_vpc_subnet" "test_vpc_subnet" {
+resource "yandex_vpc_subnet" "develop" {
   for_each = var.subnets
 
   name           = each.key
   zone           = each.value.zone
-  network_id     = yandex_vpc_network.test_vpc.id
+  network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = [each.value.cidr]
   description    = "vpc description"
 
